6021783388c0e0595f0fcbcdef774af945752e95
Tyler Parker <btylerparker@gmail.com>
2012-11-24 18:19:08 -0500

    More Vector3 functionality (and test coverage)

--- Vector3.hs
+++ Vector3.hs
@@ -6,10 +6,10 @@ module Vector3 where
 data Vector3 = Vector3 !Double !Double !Double deriving (Eq, Show)
 
 add :: Vector3 -> Vector3 -> Vector3
-(Vector3 x y z) `add` (Vector3 a b c) = Vector3 (a+x) (b+y) (c+z)
+(Vector3 a1 a2 a3) `add` (Vector3 b1 b2 b3) = Vector3 (a1+b1) (a2+b2) (a3+b3)
 
 sub :: Vector3 -> Vector3 -> Vector3
-(Vector3 x y z) `sub` (Vector3 a b c) = Vector3 (x-a) (y-b) (z-c)
+(Vector3 a1 a2 a3) `sub` (Vector3 b1 b2 b3) = Vector3 (a1-b1) (a2-b2) (a3-b3)
 
 neg :: Vector3 -> Vector3
 neg (Vector3 x y z) = (Vector3 (-x) (-y) (-z))
@@ -18,10 +18,10 @@ mult :: Vector3 -> Double -> Vector3
 mult (Vector3 x y z) c = (Vector3 (x*c) (y*c) (z*c))
 
 dot :: Vector3 -> Vector3 -> Double
-dot (Vector3 x y z) (Vector3 a b c) = x*a + b*y + c*z
+dot (Vector3 a1 a2 a3) (Vector3 b1 b2 b3) = a1*b1+a2*b2+a3*b3
 
 cross :: Vector3 -> Vector3 -> Vector3
-cross (Vector3 a b c) (Vector3 x y z) = (Vector3 (b*z + c*y) (-1*(a*z + c*x)) (a*y + b*x))
+cross (Vector3 a1 a2 a3) (Vector3 b1 b2 b3) = (Vector3 (a2*b3-a3*b2) (a3*b1-a1*b3) (a1*b2-a2*b1))
 
 mag :: Vector3 -> Double
 mag = sqrt . sq_mag
@@ -33,3 +33,13 @@ normalize :: Vector3 -> Vector3
 normalize v
   | (mag v) /= 0 = mult v (1 / mag v) 
   | otherwise    = (Vector3 0 0 0)
+                   
+angle_between :: Vector3 -> Vector3 -> Double
+angle_between a b 
+  | result < -1 = acos (-1) -- clamping to avoid floating point issues
+  | result > 1 = acos 1
+  | otherwise = acos result
+                where result = (normalize a) `dot` (normalize b)
+                      
+proj :: Vector3 -> Vector3 -> Double
+proj a b = (mag a) * (cos (angle_between a b))
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -4,6 +4,10 @@ import Test.QuickCheck
 import Control.Exception (evaluate)
 import Vector3
 
+xUnit = Vector3 1 0 0
+yUnit = Vector3 0 1 0
+zUnit = Vector3 0 0 1
+
 v0 = Vector3 0 0 0
 v1 = Vector3 5 5 5
 v2 = Vector3 5.0 5.0 5.0
@@ -12,10 +16,10 @@ v4 = Vector3 2 2 2
 v5 = Vector3 (-1) (-1) (-1)
 v6 = Vector3 3 (-4) (-1)
 v7 = Vector3 0 5 2
-
-xUnit = Vector3 1 0 0
-yUnit = Vector3 0 1 0
-zUnit = Vector3 0 0 1
+v8 = Vector3 2 1 (-1)
+v9 = Vector3 (-3) 4 1
+v10 = Vector3 5 1 11
+v11 = Vector3 (-5) (-1) (-11)
 
 main :: IO ()
 main = hspec $ do
@@ -63,3 +67,68 @@ main = hspec $ do
       
     it "should multiply by negative scalar" $ do
       (v3 `mult` (-1)) `shouldBe` v5
+      
+  -- Dot Product Tests
+  describe "Vector3 (Dot Product)" $ do
+    it "should return same value regardless of Vector3 input order" $ do
+      ((v9 `dot` v10)==(v10 `dot` v9)) `shouldBe` True
+    
+    it "should return zero when dot product'd with zero vector" $ do
+      (v1 `dot` v0) `shouldBe` 0
+      
+  -- Cross Product Tests
+  describe "Vector3 (Cross Product)" $ do
+    it "should return the negative of Vector3 (a cross b) for Vector3 (b cross a)" $ do
+      (v8 `cross` v9) `shouldBe` neg (v9 `cross` v8)
+      
+    it "should return the zero vector if a vector is crossed with the zero vector" $ do
+      (v8 `cross` v0) `shouldBe` v0
+
+  -- Magnitude Tests
+  describe "Vector3 (Magnitude)" $ do
+    it "should return zero for zero vector" $ do
+      mag v0 `shouldBe` 0
+      
+    it "should return 1 for unit vector" $ do
+      mag xUnit `shouldBe` 1
+      
+    it "should return 1 for negative unit vector" $ do
+      mag (neg xUnit) `shouldBe` 1
+    
+  -- Normalization Tests
+  describe "Vector3 (Normalize)" $ do
+    it "should have magnitude of 1 (for non zero vector)" $ do
+      mag (normalize v9) `shouldBe` 1
+  
+    it "should return zero vector given zero vector" $ do
+      normalize v0 `shouldBe` v0
+
+    it "should return xUnit vector given xUnit vector" $ do
+      normalize xUnit `shouldBe` xUnit
+      
+    it "should not return ones vector given ones vector" $ do
+      (normalize v1)==v1 `shouldBe` False
+
+
+  -- Angle Between Tests
+  describe "Vector3 (Angle Between)" $ do
+    it "should return zero given two equal vectors" $ do
+      angle_between v6 v6 `shouldBe` 0
+  
+    it "should return pi given a vector and its negative" $ do
+      angle_between v6 (neg v6) `shouldBe` pi
+  
+  
+  -- Scalar Projection Tests
+  describe "Vector3 (Scalar Projection)" $ do
+    it "should return magnitude of input vector if said vector is projected on itself" $ do
+      proj v6 v6 `shouldBe` mag v6
+  
+    it "should return zero if a vector is projected along the zero vector" $ do
+      (proj v6 v0) < 0.000001 `shouldBe` True
+      
+    it "should return zero if the zero vector is projected along another vector" $ do
+      (proj v0 v9) < 0.000001 `shouldBe` True
+      
+      
+
